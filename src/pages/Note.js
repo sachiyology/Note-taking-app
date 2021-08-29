9d38f9c6626a20355ffad0586e90c4b7b345f223
sachiyology <sachiyology@gmail.com>
2021-08-29 13:21:50 +0900

    fix some errors

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Note(props) {
 	const [notes, setNotes] = useState([]); // <==== Notes State
+	const [token, setToken] = useState('');
 
 	useEffect(() => {
 		(async () => {
@@ -19,20 +20,25 @@ export default function Note(props) {
 	return (
 		<div className="NotePage" className="list-group">
 			<ul>
-				{notes.map(note => {
-					return (
-						<li key={note._id}>
-							<Link
-								to={`/${note._id}`}
-								className="list-group-item list-group-item-action flex-column align-items-start"
-							>
-								<h3 className="mb-1">{note.title}</h3>
+				<div>
+					{token && loggedInUser === 'adminuser'
+						? notes.map(note => {
+								return (
+									<li key={note._id}>
+										<Link
+											to={`/${note._id}`}
+											className="list-group-item list-group-item-action flex-column align-items-start"
+										>
+											<h3 className="mb-1">{note.title}</h3>
 
-								<p>{note.noteBody}</p>
-							</Link>
-						</li>
-					);
-				})}
+											<p>{note.noteBody}</p>
+										</Link>
+									</li>
+								);
+						  })
+						: ''}
+				</div>
+				　
 			</ul>
 		</div>
 	);
